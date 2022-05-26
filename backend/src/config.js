ba6f101ed2f939ef38241407b94322884df32c6d
Robert.G <ghejanrobert@hotmail.com>
2022-05-26 08:44:06 +0300

    updated generic metadata

--- backend/src/config.js
+++ backend/src/config.js
@@ -75,7 +75,7 @@ const PRESALE_WHITELISTED_ADDRESSES = ["0x9154A58c7FBBA4319C21c994DEd5f53D87CbA6
 let CONTRACT_ADDRESS = "0xB0AD77206fc1E40d8baF5CF3F449C7c411B450dC"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = null; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "Which agent will you get?"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeiftmbdcdje6yeruptkgjadujvanszynatqbeg7pr3ew3go6juiire"; // Replace with your generic image that will display for all NFTs pre-reveal.
